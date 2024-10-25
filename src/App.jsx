9de4e8421f43f09bf31337c82920ec4557a57dae
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-10-25 20:07:37 +0600

    memorable water bottle project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+// 41-5 (Recap) Create Shopping Cart to handle Event with parameters
+// 41-6 [optional] Intro to Local storage to store data just one item
+// 41-7 [optional] use effect dependency and load storage id
+// 41-8 [optional] Display Saved cart from Local storage
+import './App.css'
+import Bottles from './Components/Bottles/Bottles'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Bottles></Bottles>
+      
+      
+    </>
+  )
+}
+
+export default App
